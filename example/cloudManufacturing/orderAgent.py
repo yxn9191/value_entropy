b9454685ef13bb520e74389d0619c2e9f1397f19
yxn881 <317172779@qq.com>
2022-10-14 21:50:13 +0800

    约束条件完成

--- example/cloudManufacturing/orderAgent.py
+++ example/cloudManufacturing/orderAgent.py
@@ -27,7 +27,7 @@ class OrderAgent(Agent):
         self.order_type = order_type  # 订单类型 A\B\C
         self.order_difficulty = int(order_difficulty)  # 订单难度 三个正整数 1\2\3 1为最简单
         self.cooperation = cooperation  # 订单是否支持合作（0禁止，1支持）
-        self.energy = energy  # 订单的利润
+        self.energy = energy  # 订单的价值
         self.handling_time = handling_time  # 订单预计处理时间 4-step
         self.max_duration = max_duration  # 订单的生命周期（超出后未接单会消失），10-step
         self.left_duration = self.max_duration  # 订单的剩余时间
@@ -35,8 +35,23 @@ class OrderAgent(Agent):
         self.order_satisfaction = 0  # 订单方的满意度
 
     def match_vector(self, order_type, order_difficulty):
-        self.skills[order_type] = order_type
-        self.skills[order_difficulty] = order_difficulty
+        if order_type == "A":
+            self.skills = [[1, 0, 0]]
+        elif order_type == "B":
+            self.skills = [[0, 1, 0]]
+        elif order_type == "C":
+            self.skills = [[0, 0, 1]]
+        else:
+            self.skills = [[0, 0, 0]]  # 出错，000无法与任何企业匹配
+
+        if order_difficulty == 1:
+            self.skills.append([0, 0, 1])
+        elif order_difficulty == 2:
+            self.skills.append([0, 1, 0])
+        elif order_difficulty == 3:
+            self.skills.append([1, 0, 0])
+        else:
+            self.skills.append([0, 0, 0])  # 出错，000无法与任何企业匹配
 
     def step(self, actions=None):
         pass
